6d4ac5a2cf085f60a85982e524a48432d8cde89b
leo <qduwml@163.com>
2026-04-16 18:52:32 +0800

    fix: update

--- src/HomePage.vue
+++ src/HomePage.vue
@@ -130,20 +130,45 @@
         <el-form-item label="应用名称" required>
           <el-input v-model="favoriteForm.name" placeholder="请输入应用名称"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-upload
+            :class="{
+              'avatar-uploader': true,
+              disabledPic: disabled
+            }"
+            ref="uploadFiles"
+            :limit="1"
+            accept=".jpg,.jpeg,.bmp,.png"
+            name="file"
+            :headers="headers"
+            list-type="picture-card"
+            :action="'#'"
+            :file-list="fileListNo"
+            :disabled="disabled"
+            :http-request="uploadFile"
+            :before-upload="beforeUpload"
+            :on-progress="handleFileUploadProgress"
+            :on-preview="handlePreview"
+            :on-success="handleFileSuccess"
+            :on-remove="handleRemove"
+            :auto-upload="true"
+            v-loading="showLoading"
+          >
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :modal-append-to-body="true" append-to-body :close-on-click-modal="false" :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+        </el-form-item>
+        
         <el-form-item label="URL" required>
           <el-input v-model="favoriteForm.url" placeholder="请输入URL地址"></el-input>
         </el-form-item>
-        <el-form-item label="图标">
-          <div class="icon-upload-area" @click="handleIconUpload">
-            <i class="el-icon-plus"></i>
-          </div>
-          <div class="icon-upload-tip">图标格式支持jpg、jpeg、png，图片文件大小不超过100k</div>
-        </el-form-item>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="handleCancelAddFavorite">取消</el-button>
+          <el-button type="primary" @click="handleAddFavorite">确定</el-button>
+        </div>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="handleCancelAddFavorite">取消</el-button>
-        <el-button type="primary" @click="handleAddFavorite">确定</el-button>
-      </div>
     </el-dialog>
   </div>
 </template>
@@ -170,7 +195,14 @@ export default {
         name: '',
         icon: '',
         url: ''
-      }
+      },
+      // 图标上传相关
+      fileListNo: [],
+      dialogVisible: false,
+      dialogImageUrl: '',
+      disabled: false,
+      showLoading: false,
+      headers: {}
     }
   },
   mounted() {
@@ -383,9 +415,71 @@ export default {
       // 关闭弹窗
       this.showAddFavoriteDialog = false;
     },
-    handleIconUpload() {
-      console.log('Upload icon');
-      // 实现图标上传逻辑
+    uploadFile(params) {
+      const file = params.file
+
+      const formData = new FormData()
+      formData.append("file", file)
+
+      // 这里应该调用实际的上传API
+      // uploadFile1(formData).then(res=>{
+      //   params.onSuccess(res)
+      // }).catch(error=>{
+      //   params.onError(error)
+      // })
+      
+      // 模拟上传成功
+      setTimeout(() => {
+        const mockResponse = {
+          data: {
+            originalName: file.name,
+            link: URL.createObjectURL(file)
+          }
+        };
+        params.onSuccess(mockResponse);
+      }, 1000);
+    },
+    beforeUpload(file) {
+      let acceptType = ['jpg','jpeg','png','PNG','bmp'];
+      let extension = file.name.substring(file.name.lastIndexOf('.') + 1);
+      if (-1 === acceptType.indexOf(extension)) {
+        this.$message.warning('支持文件格式'+ acceptType +'！');
+        return false;
+      }
+      if (file.size/1024 > 100) {
+        this.$message.warning("当前文件超过100k，请修改！");
+        return false;
+      }
+    },
+    handleFileUploadProgress(event, file, fileList) {
+      // 处理上传进度
+      console.log('Upload progress:', event);
+    },
+    handlePreview(file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    handleFileSuccess(response, file, fileList) {
+      let params = {
+        fileName: response.data.originalName||'',
+        fileType: '',
+        fileUrl: response.data.link||'',
+        name:  response.data.originalName||'',
+        url: response.data.link||'',
+        status: null,
+      }
+      // 清空之前的文件列表，只保留最新的
+      this.fileListNo = [params];
+      // 更新favoriteForm中的icon
+      this.favoriteForm.icon = response.data.link||'';
+      this.dialogImageUrl = file.url
+    },
+    handleRemove(file, fileList) {
+      // 处理文件移除
+      this.fileListNo = fileList;
+      if (fileList.length === 0) {
+        this.favoriteForm.icon = '';
+      }
     }
   }
 }
